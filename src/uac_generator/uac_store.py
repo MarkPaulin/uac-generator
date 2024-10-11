e1f4818a196eb8563e190a3c6f3aa487fddd5831
Mark Paulin <mark.paulin@nisra.gov.uk>
2024-10-11 15:29:25 +0100

    add sql uac store

--- src/uac_generator/uac_store.py
+++ src/uac_generator/uac_store.py
@@ -1,6 +1,8 @@
 import csv
 from pathlib import Path
 
+import pyodbc
+
 
 class UacExistsError(Exception):
     pass
@@ -38,7 +40,64 @@ class FileUacStore(UacStore):
         if self.uac_exists(uac):
             raise UacExistsError
         self.uacs.add(uac)
-        with open(self.file, "a") as f:
+        with open(self.file, "a", newline="") as f:
             new_row = [uac, *case_info] if case_info else [uac]
             filewriter = csv.writer(f)
             filewriter.writerow(new_row)
+
+
+class SqlUacStore(UacStore):
+    def __init__(self, connection_string: str, table_name: str, uac_length: int, case_info_columns: dict = None):
+        self.connection_string = connection_string
+        self.table_name = table_name
+        uac_col = {"uac": f"nchar({uac_length}) not null"}
+        self.columns = uac_col | case_info_columns if case_info_columns else uac_col
+        self.connections = []
+        self.check_table_exists()
+        self.uacs = set(self.get_all_uacs())
+
+    def connect(self):
+        if self.connections:
+            return self.connections[0]
+        else:
+            con = pyodbc.connect(self.connection_string)
+            self.connections.append(con)
+            return con
+        
+    def check_table_exists(self):
+        con = self.connect()
+        cur = con.cursor()
+        if not cur.tables(table=self.table_name, tableType='TABLE').fetchone():
+            column_statements = ",\n".join(f"{key} {value}" for key, value in self.columns.items())
+            create_table_query = f"""
+                create table {self.table_name} (
+                    {column_statements},
+                    primary key (uac)
+                )
+            """
+            cur.execute(create_table_query)
+            cur.commit()
+
+    def get_all_uacs(self):
+        con = self.connect()
+        cur = con.cursor()
+        cur.execute(f"select uac from {self.table_name}")
+        uacs = [row[0] for row in cur.fetchall()]
+        return uacs
+
+    def add(self, uac: str, case_info: list = None) -> None:
+        if self.uac_exists(uac):
+            raise UacExistsError
+            
+        insert_template = f"""
+        insert into {self.table_name} ({", ".join(self.columns)}) 
+        values ({", ".join("?" for col in self.columns)})
+        """
+        
+        new_row = [uac, *case_info] if case_info else [uac]
+        con = self.connect()
+        cur = con.cursor()
+        cur.execute(insert_template, *new_row)
+        cur.commit()
+        self.uacs.add(uac)
+ 
